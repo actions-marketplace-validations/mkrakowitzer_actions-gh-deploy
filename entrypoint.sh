63c2b4a8cc38d51ee2c05cd22b509113da46f5dd
Merritt Krakowitzer <merritt.krakowitzer@camelotglobal.com>
2019-11-27 11:37:45 +0000

    Install curl + initial update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,19 @@
 GITHUB_URL="https://github.com"
 GITHUB_API_URL="https://api.github.com"
 
-ENVIRONMENT=$1
-TASK=$2
-DESCRIPTION=$3
-TRANSIENT=$4
-
 # See https://developer.github.com/v3/repos/deployments/#create-a-deployment
 curl --silent --show-error --fail \
     -X POST "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/deployments" \
-    -H "Authorization: token ${GITHUB_TOKEN}" \
+    -H "Authorization: token ${INPUT_TOKEN}" \
     -H "Content-Type: text/json; charset=utf-8" \
     -H "Accept: application/vnd.github.ant-man-preview+json" \
     -d @- <<EOF
 {
   "ref": "${GITHUB_SHA}",
-  "environment": "${ENVIRONMENT}",
-  "task": "${TASK}",
-  "description": "${DESCRIPTION}",
-  "transient_environment": ${TRANSIENT}
+  "auto_merge": false,
+  "environment": "${INPUT_ENVIRONMENT}",
+  "task": "${INPUT_TASK}",
+  "description": "${INPUT_DESCRIPTION}",
+  "required_contexts": []
 }
 EOF
